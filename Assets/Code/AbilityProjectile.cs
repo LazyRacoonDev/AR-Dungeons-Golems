811cf0a0f42eec6ccafc497a4d6bda16e8499697
Hugo Castaño Cuéllar <114944630+LazyRacoonDev@users.noreply.github.com>
2025-04-23 01:19:31 +0200

    Proyectil

--- Assets/Code/AbilityProjectile.cs
+++ Assets/Code/AbilityProjectile.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class AbilityProjectile : MonoBehaviour
 {
     public float speed = 10f;
+    public float damage = 10f;
     private GameObject player;
     private Vector3 targetDirection;
 
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+
         if (player != null)
         {
             targetDirection = (player.transform.position - transform.position).normalized;
@@ -18,5 +20,19 @@ public class AbilityProjectile : MonoBehaviour
                 rb.velocity = targetDirection * speed;
             }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage((int)damage);
+            }
+        }
+
+        Destroy(gameObject); 
     }
 }
