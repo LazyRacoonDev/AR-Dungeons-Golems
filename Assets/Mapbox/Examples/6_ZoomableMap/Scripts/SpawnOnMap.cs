e2f4e46dd72001e84b6edfb454c21cc131763627
NachoMoren <79315360+NachoMoren@users.noreply.github.com>
2025-04-23 02:33:28 +0200

    PORFIN mapa spawnea lugares random

--- Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -1,57 +1,89 @@
-﻿namespace Mapbox.Examples
+﻿using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnAroundPlayer : MonoBehaviour
 {
-	using UnityEngine;
-	using Mapbox.Utils;
-	using Mapbox.Unity.Map;
-	using Mapbox.Unity.MeshGeneration.Factories;
-	using Mapbox.Unity.Utilities;
-	using System.Collections.Generic;
+    [SerializeField] AbstractMap _map;
+    [SerializeField] GameObject _markerPrefab;
+    [SerializeField] float _spawnScale = 100f;
+    [SerializeField] int _numberOfPoints = 10;
+    [SerializeField] float _spawnRadiusInMeters = 500f;
+    [SerializeField] float _regenerationInterval = 10f;
 
-	public class SpawnOnMap : MonoBehaviour
-	{
-		[SerializeField]
-		AbstractMap _map;
+    List<GameObject> _spawnedObjects = new List<GameObject>();
 
-		[SerializeField]
-		[Geocode]
-		string[] _locationStrings;
-		Vector2d[] _locations;
+    private bool _mapReady = false;
 
-		[SerializeField]
-		float _spawnScale = 100f;
+    void OnEnable()
+    {
+        _map.OnInitialized += StartSpawning;
+    }
 
-		[SerializeField]
-		GameObject _markerPrefab;
+    void OnDisable()
+    {
+        _map.OnInitialized -= StartSpawning;
+    }
 
-		List<GameObject> _spawnedObjects;
+    void StartSpawning()
+    {
+        _mapReady = true;
+        StartCoroutine(RegeneratePointsRoutine());
+    }
 
-		void Start()
-		{
-			_locations = new Vector2d[_locationStrings.Length];
-			_spawnedObjects = new List<GameObject>();
-			for (int i = 0; i < _locationStrings.Length; i++)
-			{
-				var locationString = _locationStrings[i];
-				_locations[i] = Conversions.StringToLatLon(locationString);
-				var instance = Instantiate(_markerPrefab);
-				instance.GetComponent<EventPointer>().eventPos = _locations[i];
+    private IEnumerator RegeneratePointsRoutine()
+    {
+        while (true)
+        {
+            // Eliminar los anteriores
+            foreach (var obj in _spawnedObjects)
+            {
+                Destroy(obj);
+            }
+            _spawnedObjects.Clear();
+
+            Vector2d playerLatLon = _map.CenterLatitudeLongitude;
+
+            for (int i = 0; i < _numberOfPoints; i++)
+            {
+                Vector2d randomLatLon = GenerateRandomPointAround(playerLatLon, _spawnRadiusInMeters);
+
+                var instance = Instantiate(_markerPrefab);
+                instance.GetComponent<EventPointer>().eventPos = randomLatLon;
                 instance.GetComponent<EventPointer>().eventID = i + 1;
-                instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				_spawnedObjects.Add(instance);
-			}
-		}
+                instance.transform.localPosition = _map.GeoToWorldPosition(randomLatLon, true);
+                instance.transform.localScale = Vector3.one * _spawnScale;
 
-		private void Update()
-		{
-			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
-			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-			}
-		}
-	}
+                _spawnedObjects.Add(instance);
+            }
+
+            yield return new WaitForSeconds(_regenerationInterval);
+        }
+    }
+
+    private void Update()
+    {
+        if (!_mapReady) return;
+
+        foreach (var obj in _spawnedObjects)
+        {
+            var location = obj.GetComponent<EventPointer>().eventPos;
+            obj.transform.localPosition = _map.GeoToWorldPosition(location, true);
+        }
+    }
+
+    Vector2d GenerateRandomPointAround(Vector2d center, float radiusInMeters)
+    {
+        float radiusInDegrees = radiusInMeters / 111320f;
+        float angle = Random.Range(0f, Mathf.PI * 2);
+        float distance = Random.Range(0f, radiusInDegrees);
+
+        float latOffset = distance * Mathf.Cos(angle);
+        float lonOffset = distance * Mathf.Sin(angle) / Mathf.Cos((float)center.x * Mathf.Deg2Rad);
+
+        return new Vector2d(center.x + latOffset, center.y + lonOffset);
+    }
 }
